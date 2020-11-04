09d0dd00ef146e2edf4a6149c953a4f0a330aa62
Angelo <angelo24782@gmail.com>
2020-11-04 15:45:23 +0100

    Added Template AllPosts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-/* exports.createPages = async function ({ actions, graphql }) {
-  const { data } = await graphql`
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
     query {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
@@ -12,30 +12,29 @@
         }
       }
     }
-  `
+  `)
 
   // Created paginated pages for posts
 
   const postPerPage = 3
   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
 
-  Array.from({length:numPages}).forEach((_, i) => {
-      actions.createPages({
-          path: i === 0 ? `/` : `/${i + 1}`,
-          component: require.resolve("./src/templates/allPosts.js"),
-          context: {
-              limit: postPerPage,
-              skip: i * postPerPage,
-              numPages,
-              currentPage: i + 1,
-          }
-      })
+  Array.from({ length: numPages }).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: require.resolve("./src/templates/allPosts.js"),
+      context: {
+        limit: postPerPage,
+        skip: i * postPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
   })
 
+  // Create single blog posts
 
-// Create single blog posts
-
-data.allMdx.edges.forEach(edge => {
+  /* data.allMdx.edges.forEach(edge => {
     const slug = edge.node.frontmatter.slug
     const id = edge.node.id
     actions.createPages({
@@ -43,7 +42,5 @@ data.allMdx.edges.forEach(edge => {
         component: require.resolve(`./src/templates/singlePost.js`),
         context: {id},
     })
-})
-
+}) */
 }
- */
